963e6bf792b101d7e99134ca9646d58daf4c325c
Musyani Luckson <musyaniluckyson@gmail.com>
2024-06-03 00:13:07 +0200

    version 2 api

--- Routes/auth_routes.js
+++ Routes/auth_routes.js
@@ -2,7 +2,7 @@ const { Router } = require(`express`);
 const router = Router();
 const Controllers = require(`../Controllers/auth_controllers`);
 
-const preReq = `/rent_a_movie`
+const preReq = `/v2`
 
 router.post(`${preReq}/signup`, Controllers.signup_post);
 router.post(`${preReq}/login`, Controllers.login_post);
--- Routes/rent_a_movie_routes.js
+++ Routes/rent_a_movie_routes.js
@@ -3,7 +3,7 @@ const router = Router();
 const Controllers = require(`../Controllers/rent_a_movie_controllers`);
 
 
-const preReq = `/rent_a_movie`;
+const preReq = `/v2`;
 router.get(`${preReq}/customer_list`, Controllers.customer_list);
 router.get(`${preReq}/transaction_list`, Controllers.transaction_list);
 
